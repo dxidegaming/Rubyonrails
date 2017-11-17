3b8e8057366c771ad7c9248a1be7101db12326c5
Edison Enerio <edison.enerio07@yahoo.com>
2017-11-16 23:09:51 -0500

    GOT IT WORKING!
    finally!

---
+++ DxideForum/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DxideForum::Application.config.secret_key_base = '0f9a53657f1d828cda3d82fe111d77d29ba3945605e6449e585f3711e8fce4d15941825e34e855b4c6647dae983626001dd82ff096bef6b37106935b74cc846b'
