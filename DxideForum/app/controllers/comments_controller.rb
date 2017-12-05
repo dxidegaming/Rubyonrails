11788b6d075a07d780951bbb883805996e08213c
dxidegaming <31665473+dxidegaming@users.noreply.github.com>
2017-12-05 14:03:31 -0500

    v2

--- DxideForum/app/controllers/comments_controller.rb
+++ DxideForum/app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to viewthread_path(@viewthread)
   end
 
+  def destroy
+    @viewthread = Viewthread.find(params[:viewthread_id])
+    @comment = @viewthread.comments.find(params[:id])
+    @comment.destroy
+    redirect_to viewthread_path(@viewthread)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
--- DxideForum/app/models/viewthread.rb
+++ DxideForum/app/models/viewthread.rb
@@ -1,6 +1,9 @@
 class Viewthread < ActiveRecord::Base
   belongs_to :User
-  has_many :comments
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 
   def created_by
     User.article_owner_email id
